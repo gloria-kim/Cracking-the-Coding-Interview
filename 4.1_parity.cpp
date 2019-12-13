cffba10569111fa8aaaae8c2422bcaa64429aace
gloria-kim <51448370+gloria-kim@users.noreply.github.com>
2019-12-12 19:12:18 -0600

    Update 4.1_parity.cpp
    Added a bool function to detect AND (&)

--- 4.1_parity.cpp
+++ 4.1_parity.cpp
@@ -1,7 +1,21 @@
 //bitwise operations
-//& (bitwise AND) = 1 if both bits are 1
-//| (bitwise OR) = 0 if both bits are 0
-//^ (bitwise XOR) 0 if both bits are same; 1 if different
+//& (bitwise AND) = 1 if both bits are 1; compare EVERY bit
+//| (bitwise OR) = 0 if both bits are 0; compare EVERY bit
+//^ (bitwise XOR) 0 if both bits are same; 1 if different; compare EVERY bit
+
+//bitwise AND
+bool AND(char a, char b)
+{
+  if (a & b == 1)
+  {
+    return true;
+  }
+  if (a & b == 0)
+  {
+    return false;
+  }
+}
+
 
 //test bits one-at-a-time
 short CountBits(unsigned int x)
@@ -13,4 +27,13 @@ short CountBits(unsigned int x)
     x >>= 1;
   }
   return num_bits;
+}
+
+int main()
+{
+  AND and;
+  bool result;
+  result = and(3,5);
+  print result;
+
 }
